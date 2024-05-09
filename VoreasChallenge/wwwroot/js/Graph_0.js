3ccb940b1e0315c2dbb8507b255afb51a143d0d3
ka_satou <ka_satou@globals.jp>
2024-05-09 13:37:42 +0900

    新規登録

---
+++ VoreasChallenge/wwwroot/js/Graph_0.js
@@ -0,0 +1,48 @@
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', '年齢(歳)');
+	data.addColumn('number', '身長(cm)');
+	data.addColumn('number', '体重(kg)');
+
+	data.addRows([
+		['1', 76, 10],
+		['2', 85, 12],
+		['3', 94, 14],
+		['4', 100, 16],
+		['5', 105, 18],
+		['6', 112, 20],
+		['7', 120, 22],
+		['8', 125, 25],
+		['9', 130, 29],
+		['10', 135, 32],
+		['11', 140, 38],
+		['12', 149, 41],
+		['13', 156, 48],
+		['14', 161, 52],
+		['15', 168, 58],
+		['16', 169, 60],
+		['17', 171, 62]
+	]);
+
+	  var options = {
+		title: '標準身長・体重曲線 男子(SD表示)',
+		curveType: 'function',
+		legend: { position: 'top' },
+		series: [
+			{ type: 'line', targetAxisIndex: 0 },
+			{ type: 'line', targetAxisIndex: 1 }
+		],
+		vAxes: [
+			{ title: '身長(cm)' }, 
+			{ title: '体重(kg)' }
+		]
+	};
+
+	var chart = new google.visualization.LineChart(document.getElementById('taikaku_chart'));
+	chart.draw(data, options);
+}
+ 
---
+++ VoreasChallenge/wwwroot/js/Graph_6.js
@@ -0,0 +1,46 @@
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', '年齢(歳)');
+	data.addColumn('number', '1.5SD');
+	data.addColumn('number', '0.5SD');
+	data.addColumn('number', '-0.5SD');
+	data.addColumn('number', '-1.5SD');
+
+	data.addRows([
+		['3', 5.0, 5.2, 5.5, 5.8],
+		['4', 4.8, 5.0, 5.2, 5.5],
+		['5', 4.6, 4.8, 5.0, 5.3],
+		['6', 4.4, 4.6, 4.8, 5.0],
+		['7', 4.2, 4.4, 4.6, 4.8],
+		['8', 4.0, 4.2, 4.4, 4.6],
+		['9', 4.0, 4.1, 4.2, 4.4],
+		['10', 3.9, 4.0, 4.1, 4.2],
+		['11', 3.8, 3.9, 4.0, 4.1],
+		['12', 3.7, 3.8, 3.9, 4.0],
+		['13', 3.6, 3.7, 3.8, 3.9],
+		['14', 3.4, 3.5, 3.6, 3.7],
+		['15', 3.2, 3.3, 3.4, 3.5],
+		['16', 3.2, 3.3, 3.3, 3.4],
+		['17', 3.1, 3.0, 3.2, 3.3],
+		['18', 3.0, 3.1, 3.1, 3.2],
+		['19', 3.0, 3.1, 3.1, 3.1],
+		['20', 3.0, 3.1, 3.1, 3.1],
+		['21', 2.9, 3.0, 3.0, 3.1],
+		['22', 2.9, 3.0, 3.0, 3.1]
+	]);
+
+	// Set chart options
+	var options = {
+		'title':'',
+		curveType: 'function',
+		legend: { position: 'top' }
+	};
+
+	var chart = new google.visualization.ComboChart(document.getElementById('noryoku6_chart'));
+	chart.draw(data, options);
+}
+ 
