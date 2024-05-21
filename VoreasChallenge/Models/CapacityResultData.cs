583ee26ebc575fc5b3515afdc49e28dc141a1e94
ka_satou <ka_satou@globals.jp>
2024-05-21 15:47:35 +0900

    画像中データ入力完了(3)

--- VoreasChallenge/Models/CapacityResultData.cs
+++ VoreasChallenge/Models/CapacityResultData.cs
@@ -14,15 +14,23 @@ namespace VoreasChallenge.Models
 		public int ID { get; set; }						// データID
 
 		[DataType(DataType.Date)]
-		public DateTime MeasureDay { get; set; }			// 測定日
+		public DateTime? MeasureDay { get; set; }			// 測定日
 
+		[DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = false)]
 		public float? Run20m { get; set; }					// 20m走
+		[DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = false)]
 		public float? ProAgility { get; set; }				// プロアジティ
+		[DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = false)]
 		public float? StandJump { get; set; }				// 立幅跳び
+		[DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = false)]
 		public float? RepetJump { get; set; }				// 反復横跳び
+		[DisplayFormat(DataFormatString = "{0:0.0}", ApplyFormatInEditMode = false)]
 		public float? VerticalJump { get; set; }			// 垂直跳び
+		[DisplayFormat(DataFormatString = "{0:0.0}", ApplyFormatInEditMode = false)]
 		public float? ReboundJumpIndex { get; set; }		// リバウウンドジャンプ指数
+		[DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = false)]
 		public float? GCTime { get; set; }					// 接地時間
+		[DisplayFormat(DataFormatString = "{0:0.0}", ApplyFormatInEditMode = false)]
 		public float? JumpHeight { get; set; }				// 跳躍高
 	}
 }
